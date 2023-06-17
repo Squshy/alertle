53dee41681f115ed1ab414aa4b21b8d015804699
Calvin Lapp <lapp.cd@gmail.com>
2023-06-17 19:54:55 -0400

    Modified config layout

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
-module.exports = {
+/** @type {import("eslint").Linter.Config} */
+const config = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
+  extends: ["@headless-alert"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
     },
   },
 };
+
+module.exports = config;
