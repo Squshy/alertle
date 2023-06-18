2f873c347e610ec2ba7849943653b3a8be135270
Calvin Lapp <lapp.cd@gmail.com>
2023-06-17 21:06:17 -0400

    LINTING

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,12 @@ const config = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["@headless-alert"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    tsconfigRootDir: __dirname,
+    project: ["./packages/*/tsconfig.json"],
+  },
   settings: {
     next: {
       rootDir: ["apps/*/"],
--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -7,12 +7,37 @@ const config = {
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
   ],
   parser: "@typescript-eslint/parser",
-  rules: {},
-  parserOptions: {},
+  plugins: ["@typescript-eslint", "import"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  overrides: [
+    {
+      files: ["packages/react/**"],
+      plugins: ["react", "react-hooks"],
+      extends: ["plugin:react/recommended", "plugin:react-hooks/recommended"],
+      rules: {
+        "react/react-in-jsx-scope": "off",
+      },
+      settings: {
+        react: {
+          version: "detect",
+        },
+      },
+    },
+  ],
   ignorePatterns: [
     "**/*.config.js",
     "**/*.config.cjs",
-    ".eslintrc.cjs",
+    ".eslintrc.*",
     "**/dist/**",
   ],
 };
