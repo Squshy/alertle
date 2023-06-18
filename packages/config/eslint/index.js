c0af3abdc03940fe0bf91e246ec2cbd8991e4013
Calvin Lapp <lapp.cd@gmail.com>
2023-06-17 20:15:44 -0400

    Modified eslint

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -1,5 +1,20 @@
-module.exports = {
-  extends: ["turbo", "prettier"],
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  extends: [
+    "turbo",
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  parser: "@typescript-eslint/parser",
   rules: {},
   parserOptions: {},
+  ignorePatterns: [
+    "**/*.config.js",
+    "**/*.config.cjs",
+    ".eslintrc.cjs",
+    "**/dist/**",
+  ],
 };
+
+module.exports = config;
